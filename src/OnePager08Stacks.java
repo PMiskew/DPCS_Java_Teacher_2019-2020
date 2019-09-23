9774f61ecee6e7d7c927b1c9d9bc69b6cdf8b9f7
PMiskew <paul.miskew@gmail.com>
2019-09-23 15:22:29 -0400

    OnePager 08 solutions

--- src/OnePager08Stacks.java
+++ src/OnePager08Stacks.java
@@ -5,13 +5,17 @@ public class OnePager08Stacks {
 
 	public static void main(String[] args) {
 
+		//Question 1: 
 		//For this exercise assume an ArrayList is the "COLLECTION"
+		//An extension here is think about if the question indicated which
+		//element should be at the base of the stack. 
 		ArrayList<Integer> list1 = new ArrayList<Integer>();
 		Stack<Integer> stack1 = new Stack<>();
 
 		list1.add(1);
 		list1.add(5);
 		list1.add(99);
+		list1.add(4);
 		
 		while (list1.size() > 0) {
 			stack1.push(list1.remove(0));
